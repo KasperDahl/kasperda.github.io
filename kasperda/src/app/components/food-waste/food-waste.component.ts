4df65bf7c36b2494dfa71a060cdffabc17bf3480
KasperDahl <76221662+KasperDahl@users.noreply.github.com>
2024-10-22 16:25:35 +0200

    Added navbar and components to it and global styles

---
+++ kasperda/src/app/components/food-waste/food-waste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-waste',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-waste.component.html',
+  styleUrl: './food-waste.component.scss'
+})
+export class FoodWasteComponent {
+
+}
